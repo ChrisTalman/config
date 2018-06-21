18e698a6fba8983d4a60ddf62066cce1bea4489a
Chris Talman <cewe.wow@gmail.com>
2018-06-22 00:39:48 +0100

    Introduced data property; provided description comment for initialise().

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,9 @@ declare module '@bluecewe/config'
 {
 	export default class Store <Config>
     {
+		/** The stored config object. */
+		public data(): Config;
+		/** Retrieves, parses, validates, and stores config.json. */
         public initialise(): Config;
     }
 }
